c8afb57812dd48a7c1756c3b501974d124f934bf
Rafael <rafaelhueb92@gmail.com>
2021-10-23 01:07:21 -0300

    First commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+#### Package the project to generate jar files 
+mvn package
+
+#### To build the image in Maven, you can use a simpler Docker command line:
+
+docker build -t springio/gs-spring-boot-docker .
---
+++ src/main/java/br/com/siecola/aws_project101/AwsProject101Application.java
@@ -0,0 +1,13 @@
+package br.com.siecola.aws_project101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProject101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProject101Application.class, args);
+	}
+
+}
